fb2717f9b21dfc759e15991ce8379930b43d48da
Nexus Project <136268580+nexusproject2077@users.noreply.github.com>
2025-12-02 22:06:51 +0100

    Update utils.js

--- app/frontend/src/lib/utils.js
+++ app/frontend/src/lib/utils.js
@@ -3,8 +3,7 @@
 import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
 
-function cn(...inputs) {
+export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
 
-export default cn
