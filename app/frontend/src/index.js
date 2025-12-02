3185121dabd2b2ba0d3bdb273b630935d6854e47
Nexus Project <136268580+nexusproject2077@users.noreply.github.com>
2025-12-02 10:20:22 +0100

    Update index.js

--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -2,8 +2,8 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import './index.css'; // ✅ Correction : Chemin relatif pour le CSS
-import App from "./App"; // ✅ Correction : Chemin relatif pour le composant App
+import '../index.css'; // ✅ Correction : Chemin relatif pour le CSS
+import App from "../App"; // ✅ Correction : Chemin relatif pour le composant App
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
