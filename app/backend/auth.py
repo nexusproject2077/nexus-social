6a6ee8c9b239ecab9515e60e2729343320ee8469
Nexus Project <136268580+nexusproject2077@users.noreply.github.com>
2025-12-07 17:17:03 +0100

    Create auth.py

---
+++ app/backend/auth.py
@@ -0,0 +1,26 @@
+# backend/auth.py
+from fastapi import Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from backend.server import db
+from datetime import datetime, timezone, timedelta
+import jwt
+from fastapi import HTTPException
+
+security = HTTPBearer()
+SECRET_KEY = "..."
+
+async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    try:
+        token = credentials.credentials
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        user_id = payload.get("sub")
+        if not user_id:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        user = await db.users.find_one({"id": user_id}, {"_id": 0})
+        if not user:
+            raise HTTPException(status_code=401, detail="User not found")
+        return user
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token expired")
+    except:
+        raise HTTPException(status_code=401, detail="Invalid token")
