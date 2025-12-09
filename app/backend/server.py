d387d8863878983b168e5d69cfc334f79040bd7c
Nexus Project <136268580+nexusproject2077@users.noreply.github.com>
2025-12-09 18:02:51 +0100

    Update server.py

--- app/backend/server.py
+++ app/backend/server.py
@@ -89,7 +89,7 @@ def convert_mongo_doc_to_dict(doc: dict) -> dict:
 from backend.routers import stories # Importe le routeur stories APRÈS la création de 'app' et 'db'
 
 api_router = APIRouter(prefix="/api")
-api_router.include_router(stories.router, prefix="/stories") # Inclut le routeur stories avec son préfixe
+api_router.include_router(stories.router) # Inclut le routeur stories avec son préfixe
 
 
 # ==================== MODELS ====================
